593964b3766195fde44e643d55ca2baf7f592bb0
Blessed-03 <bie03manenji@gmail.com>
2024-08-07 22:42:14 -0500

    Version 2

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import tools.Calc;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world...");
+        Calc calc1 = new Calc(3,4);
+        System.out.println(calc1.add());
+        ///System.out.println(calc1.add());
     }
 }
